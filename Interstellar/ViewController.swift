0e544c45de07e6ee437e9501f8452e4db069b56f
Harry <poripori1030@gmail.com>
2017-10-11 04:28:50 +0800

    Initial Commit

---
+++ Interstellar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Interstellar
+//
+//  Created by Harry on 11/10/2017.
+//  Copyright © 2017 devconcept. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
